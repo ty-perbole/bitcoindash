347f9e1a32f6261ff4aa698f5811b708d3019199
typerbole <typerbole@pm.me>
2020-11-07 10:23:35 -0800

    refresh shell script

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from flask import Flask, Response
 import gc
 
 import value, blockspace, security, privacy, layer2
-import data_cm, data_node, data_wp
 
 server = Flask(__name__)
 app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.SLATE])
@@ -201,9 +200,6 @@ def render_content(tab, start_date, end_date, date_granularity, log_linear):
 
 @server.route('/dataRefresh/', methods=['GET'])
 def dataRefresh():
-    data_cm.run()
-    data_node.run()
-    data_wp.run()
     sys.stdout.write('Finished all refresh jobs')
     return Response("Finished data refresh", mimetype='text/plain')
 
---
+++ resfresh_data.sh
@@ -0,0 +1,3 @@
+python data_cm.py
+python data_node.py
+python data_wp.py
