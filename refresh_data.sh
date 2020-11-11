1b563db63dd4280ada783c398713f78d3b01834a
typerbole <typerbole@pm.me>
2020-11-10 20:58:20 -0800

    update refresh script with ln data

--- refresh_data.sh
+++ refresh_data.sh
@@ -6,5 +6,34 @@ python data_cm.py
 python data_node.py
 python data_wp.py
 
+{
+  rm lnchannels.dump
+  } || {
+    echo norm
+}
+{
+  wget https://ln.bigsun.xyz/lnchannels.dump
+  dropdb ln
+  createdb ln
+  psql ln < lnchannels.dump
+  psql ln -c "\COPY (
+  SELECT
+      short_channel_id
+    , satoshis
+    , nodes::json->0 AS node1
+    , nodes::json->1 AS node2
+    , (open->>'time')::int AS open_ts
+    , (open->>'block')::int AS open_block
+    , (close->>'time')::int AS close_ts
+    , (close->>'block')::int AS close_block
+    , last_update
+  FROM channels)
+  TO '/home/admin/bitcoindash/ln_channels.csv' DELIMITER ',' CSV HEADER;"
+} || {
+  echo ln import failed
+}
+
+python data_ln.py
+
 /home/admin/google/google-cloud-sdk/bin/gsutil -m rsync ./data gs://bitcoinkpis-data/data
 /home/admin/google/google-cloud-sdk/bin/gcloud --quiet beta app deploy
